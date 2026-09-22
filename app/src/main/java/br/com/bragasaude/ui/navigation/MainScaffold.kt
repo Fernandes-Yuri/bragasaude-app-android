ca16080d485977fff712b41877460941cb6f0729
Fernandes-Yuri <workfjsyuri@gmail.com>
2026-09-22 18:28:21 -0300

    chore: restaura BOM UTF-8 do MainScaffold.kt

--- app/src/main/java/br/com/bragasaude/ui/navigation/MainScaffold.kt
+++ app/src/main/java/br/com/bragasaude/ui/navigation/MainScaffold.kt
@@ -1,4 +1,4 @@
-package br.com.bragasaude.ui.navigation
+﻿package br.com.bragasaude.ui.navigation
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
